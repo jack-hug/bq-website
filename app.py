57417650bc8f06a4095414916c27a91141e0a846
jack-hug <46361381@qq.com>
2023-06-13 17:11:01 +0800

    2023年6月13日 前端页面基础完成，后续将进行细节修改。

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 intro = {
     'introduce': '广西邦琪药业集团有限公司是一家已有20'
                  '多年的发展历史的民营制药企业，连续入榜广西民营制造业百强、广西高新技术企业百强行列，是广西优秀企业，是集科研、生产、销售、服务为一体的综合性企业集团。集团公司旗下现有10'
-                 '多家子公司，包括百琪药业、葛洪堂药业、邦琪医药、泰和制药等多家医药企业。集团公司在药品的生产销售和药品研发方面有较强的特色和优势，产品资源相当丰富，药品生产批文号350多个，独家品种35个（其中10'
+                 '多家子公司，包括百琪药业、葛洪堂药业、邦琪医药、泰和制药等多家医药企业。集团公司在药品的生产销售和药品研发方面有较强的特色和优势，产品资源相当丰富，药品生产批文号400多个，独家品种45个（其中10'
                  '个有新药证书）。主要产品有桂龙药膏、复方鱼腥草颗粒、金鸡胶囊、五参安神口服液、罗汉果止咳膏、黄荆油胶丸、抗宫炎颗粒、三参益气口服液、参芪首乌补汁、咳喘停膏等品种。'
 }
 
@@ -64,6 +64,26 @@ def introduce():
     return render_template('introduce.html')
 
 
+@app.route('/introduce_company')
+def introduce_company():
+    return render_template('introduce_company.html')
+
+
+@app.route('/introduce_quality')
+def introduce_quality():
+    return render_template('introduce_quality.html')
+
+
+@app.route('/introduce_structure')
+def introduce_structure():
+    return render_template('introduce_structure.html')
+
+
+@app.route('/introduce_responsibility')
+def introduce_responsibility():
+    return render_template('introduce_responsibility.html')
+
+
 @app.route('/honor')
 def honor():
     return render_template('honor.html')
@@ -72,6 +92,16 @@ def honor():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/contact_cooperate')
+def contact_cooperate():
+    return render_template('contact_cooperate.html')
+
+
+@app.route('/contact_recruit')
+def contact_recruit():
+    return render_template('contact_recruit.html')
 
 
 if __name__ == '__main__':
